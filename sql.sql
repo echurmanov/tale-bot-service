c2d8fac4f6ddf0214816eeab634762387d070e10
Eugene Churmanov <echurmanov@gmail.com>
2016-09-26 19:09:46 +0500

    Добавлен SQL для БД

---
+++ sql.sql
@@ -0,0 +1,19 @@
+Create Table: CREATE TABLE `users` (
+  `user_id` char(36) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `pass` varchar(64) DEFAULT NULL,
+  `status` enum('ACTIVE','DEACTIVE') DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+Create Table: CREATE TABLE `auth_requests` (
+  `token_id` varchar(255) NOT NULL,
+  `user_id` char(36) DEFAULT NULL,
+  `updateDate` datetime DEFAULT NULL,
+  `sessionid` varchar(100) DEFAULT NULL,
+  `csrftoken` varchar(255) DEFAULT NULL,
+  `state` enum('AUTH_NONE','AUTH_WAIT','AUTH_SUCCESS','AUTH_REJECTED') DEFAULT NULL,
+  `controlStatus` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`token_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
